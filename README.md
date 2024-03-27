a30d09dc45c2e0baeb61296b55a7b535a95e15b5
WalterCorsini <corsini.walter@gmail.com>
2024-03-27 17:37:44 +0100

    add project, result on page

--- README.md
+++ README.md
@@ -6,15 +6,15 @@ sostituiremo il numero in "FizzBuzz" ai numeri che sono mutlipli sia di 3 che di
 
 ## INPUT
 ### CSS
-1. - [ ] Creo delle classi per i quadrati;
-2. - [ ] Creo delle classi per lo sfondo dei quadrati Fizz, Buzz e FizzBuzz;
+- [x] Creo delle classi per i quadrati;
+- [x] Creo delle classi per lo sfondo dei quadrati Fizz, Buzz e FizzBuzz;
 ### JS
-2. - [ ] Creo un elemento con la classe base "quadrato";
+- [X] Creo un elemento "div" con la classe base "quadrato";
 ## LOGICA
-3. - [ ] creo un ciclo for da 1 a 100;
-4. - [ ] creo all'interno delle condizioni per assegnare il valore alla variabile;
-5. - [ ] assegno class sfondo all'elemento;
-6. - [ ] assegno valore all'elemento;
+- [ ] creo un ciclo for da 1 a 100;
+- [ ] creo all'interno delle condizioni per assegnare il valore alla variabile;
+- [ ] assegno class sfondo all'elemento;
+- [ ] assegno valore all'elemento;
 
 ## OUTPUT
-5. - [ ] creo elmento in pagina;
+- [ ] creo elmento in pagina;
--- js/script.js
+++ js/script.js
@@ -1,3 +1,49 @@
 // test
 // alert("ciao");
 
+// search container and save in variable
+const containerElem = document.querySelector(".container")
+
+
+// declaration result variable
+let result;
+
+// LOGIC
+// start FOR
+for(let i = 1; i <= 100; i++){
+    // reset e declaration variable
+    const divElem = document.createElement("div");
+    divElem.classList.add("box");
+    divElem.classList.remove("fizz");
+    divElem.classList.remove("fizz-buzz");
+    divElem.classList.remove("buzz");
+    result = "";
+
+    // divisible by 3 and 5
+    if(i % 3 == 0 && i % 5 == 0){
+        result = "fizzbuzz";
+        divElem.classList.add("fizz-buzz");
+        console.log(result);
+
+    // divisible by 3 and 5
+    } else if (i % 3 == 0){
+        result = "fizz";
+        divElem.classList.add("fizz");
+        bgClass= "fizz";
+
+    // if divisible by 3 and 5
+    } else if(i % 5 == 0){
+        result = "buzz";
+        divElem.classList.add("buzz");
+        bgClass= "buzz";
+    
+    // else
+    } else{
+        result = i;
+    }
+
+// OUTPUT
+    divElem.innerHTML = result;
+    containerElem.append(divElem);
+
+}
